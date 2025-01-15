fdb2d53292fd819f81c6a206f1d1c386c96ce1cd
istiak7 <istiak6069@gmail.com>
2025-01-15 17:35:09 +0600

    environment and configuration

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php 
 
 require "functions.php";
-require "router.php";
+require "Database.php";
+//require "router.php";
+$config = require "config.php";
+$db=new Database($config['database']);
+$posts=$db->query("SELECT * FROM post")->fetchAll();
+dd($posts);
